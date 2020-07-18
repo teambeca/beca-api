057bcdc7e17c9c7c513cebc98ac36223f30af77a
hahahayesss <hahahayesss@gmail.com>
2020-07-18 19:32:34 +0300

    18/07/2020 - 19:32

---
+++ src/main/java/com/r00t/becaapi/controllers/pub/SecurityController.java
@@ -0,0 +1,9 @@
+package com.r00t.becaapi.controllers.pub;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class SecurityController {
+}
