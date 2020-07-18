bf6cf57a7af0c9fcd05a259d45cbc0c8d6e95eec
Ahmet Turgut <hahahayesss@gmail.com>
2020-07-18 14:52:55 +0300

    close #TEK-7

---
+++ src/main/java/com/r00t/becaapi/BecaApiApplication.java
@@ -0,0 +1,13 @@
+package com.r00t.becaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BecaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BecaApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/r00t/becaapi/BecaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.r00t.becaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BecaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
