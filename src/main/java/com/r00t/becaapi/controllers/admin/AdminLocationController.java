8125c9f4c7ec0c5ca38cc246bde98ff791a26eae
hahahayesss <hahahayesss@gmail.com>
2020-07-30 00:59:41 +0300

    30/07/2020 - 00:59

--- src/main/java/com/r00t/becaapi/controllers/admin/AdminLocationController.java
+++ src/main/java/com/r00t/becaapi/controllers/admin/AdminLocationController.java
@@ -2,6 +2,7 @@ package com.r00t.becaapi.controllers.admin;
 
 import com.r00t.becaapi.exceptions.NotFoundException;
 import com.r00t.becaapi.models.LocationCredentials;
+import com.r00t.becaapi.models.requests.Seed;
 import com.r00t.becaapi.services.LocationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -33,6 +34,20 @@ public class AdminLocationController {
             @RequestBody LocationCredentials requestCredentials) {
         return ResponseEntity.ok().body(
                 locationService.insertCredentials(requestCredentials));
+    }
+
+    @PostMapping("/seed")
+    public ResponseEntity<?> seed(
+            @RequestBody Seed seed) {
+        seed.getTexts()
+                .forEach(s -> {
+                    LocationCredentials l = new LocationCredentials();
+                    l.setName(s);
+                    locationService.insertCredentials(l);
+                });
+
+        return ResponseEntity.ok().body(
+                Map.of("status", "ok"));
     }
 
     @PatchMapping
